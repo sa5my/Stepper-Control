0aae655c6768753094fd03fe2b4907cc38b9049f
Abdul Samad <abdul.samad5052@gmail.com>
2020-12-11 15:18:46 +0530

    Speed Period Table
    This consists of a list of time delay/period to energize the stepper motor to run at various speeds.

---
+++ MotorEnd_Device/Stepper_SpdDelays.c
@@ -0,0 +1,105 @@
+const unsigned long MtrSpd_Table[] =
+{
+  /*Speed -0.0 RPM*/
+  0,
+  /* Speed - 0.1 RPM */
+  46880, //Delay in Microseconds
+  /* Speed - 0.2 RPM */
+  23440, //Delay in Microseconds
+  /* Speed - 0.3 RPM */
+  15630, //Delay in Microseconds
+  /* Speed - 0.4 RPM */
+  11720, //Delay in Microseconds
+  /* Speed - 0.5 RPM */
+  9380, //Delay in Microseconds
+  /* Speed - 0.6 RPM */
+  7810, //Delay in Microseconds
+  /* Speed - 0.7 RPM */
+  6700, //Delay in Microseconds
+  /* Speed - 0.8 RPM */
+  5860, //Delay in Microseconds
+  /* Speed - 0.9 RPM */
+  5210, //Delay in Microseconds
+  /* Speed - 1 RPM */
+  4690, //Delay in Microseconds
+  /* Speed - 1.1 RPM */
+  4260, //Delay in Microseconds
+  /* Speed - 1.2 RPM */
+  3910, //Delay in Microseconds
+  /* Speed - 1.3 RPM */
+  3610, //Delay in Microseconds
+  /* Speed - 1.4 RPM */
+  3350, //Delay in Microseconds
+  /* Speed - 1.5 RPM */
+  3130, //Delay in Microseconds
+  /* Speed - 1.6 RPM */
+  2930, //Delay in Microseconds
+  /* Speed - 1.7 RPM */
+  2760, //Delay in Microseconds
+  /* Speed - 1.8 RPM */
+  2600, //Delay in Microseconds
+  /* Speed - 1.9 RPM */
+  2470, //Delay in Microseconds
+  /* Speed - 2 RPM */
+  2340, //Delay in Microseconds
+  /* Speed - 2.1 RPM */
+  2230, //Delay in Microseconds
+  /* Speed - 2.2 RPM */
+  2130, //Delay in Microseconds
+  /* Speed - 2.3 RPM */
+  2040, //Delay in Microseconds
+  /* Speed - 2.4 RPM */
+  1950, //Delay in Microseconds
+  /* Speed - 2.5 RPM */
+  1880, //Delay in Microseconds
+  /* Speed - 2.6 RPM */
+  1800, //Delay in Microseconds
+  /* Speed - 2.7 RPM */
+  1740, //Delay in Microseconds
+  /* Speed - 2.8 RPM */
+  1670, //Delay in Microseconds
+  /* Speed - 2.9 RPM */
+  1620, //Delay in Microseconds
+  /* Speed - 3 RPM */
+  1560, //Delay in Microseconds
+  /* Speed - 3.1 RPM */
+  1510, //Delay in Microseconds
+  /* Speed - 3.2 RPM */
+  1460, //Delay in Microseconds
+  /* Speed - 3.3 RPM */
+  1420, //Delay in Microseconds
+  /* Speed - 3.4 RPM */
+  1380, //Delay in Microseconds
+  /* Speed - 3.5 RPM */
+  1340, //Delay in Microseconds
+  /* Speed - 3.6 RPM */
+  1300, //Delay in Microseconds
+  /* Speed - 3.7 RPM */
+  1270, //Delay in Microseconds
+  /* Speed - 3.8 RPM */
+  1230, //Delay in Microseconds
+  /* Speed - 3.9 RPM */
+  1200, //Delay in Microseconds
+  /* Speed - 4 RPM */
+  1170, //Delay in Microseconds
+  /* Speed - 4.1 RPM */
+  1140, //Delay in Microseconds
+  /* Speed - 4.2 RPM */
+  1120, //Delay in Microseconds
+  /* Speed - 4.3 RPM */
+  1090, //Delay in Microseconds
+  /* Speed - 4.4 RPM */
+  1070, //Delay in Microseconds
+  /* Speed - 4.5 RPM */
+  1040, //Delay in Microseconds
+  /* Speed - 4.6 RPM */
+  1020, //Delay in Microseconds
+  /* Speed - 4.7 RPM */
+  1000, //Delay in Microseconds
+  /* Speed - 4.8 RPM */
+  980, //Delay in Microseconds
+  /* Speed - 4.9 RPM */
+  960, //Delay in Microseconds
+  /* Speed - 5 RPM */
+  940, //Delay in Microseconds
+};
